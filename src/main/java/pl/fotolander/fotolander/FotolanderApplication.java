9f3dc4be3b4e6f7e8884d24111536acac7eaf572
Zolleander <jurgon@hot.pl>
2021-06-28 15:37:39 +0200

    first commit

---
+++ src/main/java/pl/fotolander/fotolander/FotolanderApplication.java
@@ -0,0 +1,13 @@
+package pl.fotolander.fotolander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotolanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotolanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/fotolander/fotolander/FotolanderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fotolander.fotolander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotolanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
