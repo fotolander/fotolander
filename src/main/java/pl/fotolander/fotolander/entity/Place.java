494815a772dfd977c0937dad817f33d7ca02d2a0
Aleksandralucz <luczynska.aleksandraa@gmail.com>
2021-06-29 14:34:49 +0200

    added place class

---
+++ src/main/java/pl/fotolander/fotolander/entity/Place.java
@@ -0,0 +1,36 @@
+package pl.fotolander.fotolander.entity;
+/**
+ * nazwa
+ * adres
+ * lat / lon - współrzędne geograficzne
+ * opis
+ */
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Place {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String address;
+    private String description;
+
+    private double latitude;
+    private double longitude;
+
+    @EqualsAndHashCode.Exclude
+    @ManyToOne
+    private Pin pin;
+
+
+}
