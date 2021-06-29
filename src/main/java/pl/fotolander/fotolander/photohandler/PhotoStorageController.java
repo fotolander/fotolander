ebfcc0225b4c25ee6058100b0ad9a68377c10fa3
Kasia <sawicka.katarzyna91@gmail.com>
2021-06-29 21:48:25 +0300

    add controller

--- src/main/java/pl/fotolander/fotolander/photohandler/PhotoStorageController.java
+++ src/main/java/pl/fotolander/fotolander/photohandler/PhotoStorageController.java
@@ -3,6 +3,10 @@ package pl.fotolander.fotolander.photohandler;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class PhotoStorageController {
@@ -13,6 +17,16 @@ public class PhotoStorageController {
         this.photoStorageService = photoStorageService;
     }
 
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("photos", photoStorageService.getPhoto());
+        return "photos";
+    }
 
 
+    @PostMapping("/upload")
+    public String uploadPhoto(@RequestParam("photo")MultipartFile photo) {
+        photoStorageService.uploadPhoto(new UploadPhoto(photo, photo.getOriginalFilename()));
+        return "redirect:/";
+    }
 }
